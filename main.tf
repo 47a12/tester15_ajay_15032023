d23dbd3ec790152a34671c4602a88a7894509350
47a12 <99970745+47a12@users.noreply.github.com>
2023-03-15 13:09:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ output_path = "${path.module}/python/hello-python.zip"
 
 resource "aws_lambda_function" "terraform_lambda_func" {
 filename                       = "${path.module}/python/hello-python.zip"
-function_name                  = "Spacelift_Test_Lambda_Function"
+function_name                  = "terminate-instance"
 role                           = aws_iam_role.lambda_role.arn
 handler                        = "index.lambda_handler"
 runtime                        = "python3.8"
