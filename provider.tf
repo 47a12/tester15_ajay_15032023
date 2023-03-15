a3ebe4e1396a7eab416576471c278ff77235d78f
47a12 <99970745+47a12@users.noreply.github.com>
2023-03-15 13:05:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,3 +3,10 @@ provider "aws" {
 }
 
 # Add your S3 backend configuration here
+
+  backend "s3" {
+    bucket = "3.devops.candidate.exam"
+    key    = "ajay.kushwah"
+    region = "eu-west-1"
+  }
+}
