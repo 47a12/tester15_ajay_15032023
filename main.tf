f28d076d34d7c6b4cf7558317b75e4930edf2a16
47a12 <99970745+47a12@users.noreply.github.com>
2023-03-15 12:59:31 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,96 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  
+}
+
+
+resource "aws_subnet" "my-subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/16"
+
+}
+
+
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+  }
+
+}
+
+resource "aws_iam_role" "lambda_role" {
+name   = "Spacelift_Test_Lambda_Function_Role"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+ 
+ name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+ path         = "/"
+ description  = "AWS IAM Policy for managing aws lambda role"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        = aws_iam_role.lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+
+def lambda_handler(event, context):
+   message = 'Hello {} !'.format(event['key1'])
+   return {
+       'message' : message
+   }
+   
+   
+   data "archive_file" "zip_the_python_code" {
+type        = "zip"
+source_dir  = "${path.module}/python/"
+output_path = "${path.module}/python/hello-python.zip"
+}
+
+resource "aws_lambda_function" "terraform_lambda_func" {
+filename                       = "${path.module}/python/hello-python.zip"
+function_name                  = "Spacelift_Test_Lambda_Function"
+role                           = aws_iam_role.lambda_role.arn
+handler                        = "index.lambda_handler"
+runtime                        = "python3.8"
+depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
+
+
+
